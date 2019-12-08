22fb6d2bd83cd92b4fb0b823f331ec5dddc86077
André Laszlo <andre@laszlo.nu>
2019-12-08 22:52:46 +0100

    Add error handling and retries to worker

--- demo/lib/pubsub_adapter/worker.rb
+++ demo/lib/pubsub_adapter/worker.rb
@@ -28,11 +28,30 @@ module ActiveJob::PubSub
       Rails.logger.info "Waiting for jobs in the '#{@queue}' queue"
       sub = @pubsub.get_or_create_subscription(@queue)
       subscriber = sub.listen(threads: @threads) do |received_message|
-        # TODO: Error handling
         job_data = JSON.load(received_message.data)
         Rails.logger.info "Processing job #{job_data}"
-        ActiveJob::Base.execute job_data
-        received_message.acknowledge!
+
+        begin
+          ActiveJob::Base.execute job_data
+        rescue Exception => err  # Catching all here, because a worker should be robust against any errors
+          # The job failed, increment executions counter and re-enqueue it
+          begin
+            Rails.logger.warn "Job failed: #{err.inspect}"
+            job = ActiveJob::Base.deserialize job_data
+            job.executions += 1
+            job.class.perform_later job
+            Rails.logger.info "Re-enqueued failed job after #{job.executions} attempt(s)"
+            # Only ack the failed job after successfully re-enqueueing it
+            received_message.acknowledge!
+          rescue Exception => error_handling_err
+            Rails.warn "An error occured while handling a failed job, "\
+                       "this job will not be acked and retried according to "\
+                       "the pubsub subscription's parameters: #{error_handling_err}"
+          end
+        else
+          received_message.acknowledge!
+        end
+
       end
       subscriber.start
       sleep
